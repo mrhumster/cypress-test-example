7e64ea29662949bfbf2a068e494502fde40c3aaa
XOMRKOB <ru89050995432@yandex.ru>
2023-11-07 10:01:21 +0600

    end test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
+    cy.contains('Customer Login').click();
+    cy.get('#userSelect').select('Harry Potter');
+    cy.contains('Login').click();
+    cy.contains('Deposit').click();
+    cy.get('.form-control').type('100');
+    cy.get('button.btn-default').click();
+    cy.get('span.error').should('contain','Deposit Successful');
+    cy.get('button.logout').click();
   })
 })
+
